95ca9e81c91a15b3c6f2decc4abec954849ad508
FerbaDev <elferbarron1@gmail.com>
2023-07-24 00:35:35 -0300

    logica de ganador y modal de anuncio

--- src/App.jsx
+++ src/App.jsx
@@ -19,17 +19,56 @@ const Square = ({ children, updateBoard, index, isSelected }) => {
   );
 };
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
+
   const [turn, setTurn] = useState(TURNS.X);
+
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    return null;
+  };
+
+  //actualiza el tablero
   const updateBoard = (index) => {
-    if (board[index]) return;
+    if (board[index] || winner) return;
+
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
+
+    //cambia el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    //revisar si hay ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
   };
+
   return (
     <main className="board">
       <h1>ta te ti</h1>
@@ -46,6 +85,20 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+
+      {winner !== null && (
+        <section className="winner">
+          <div className="text">
+            <h2>{winner === false ? `Empate` : `Ganó: `}</h2>
+            <header className="win">
+              {winner && <Square>{winner}</Square>}
+            </header>
+            <footer>
+              <button>Empezar de nuevo</button>
+            </footer>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
